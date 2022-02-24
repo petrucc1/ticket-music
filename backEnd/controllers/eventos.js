8aa9d0ded364655f3e43919e3f5e1e89dffe567c
otavio augusto <otavio95123@gmail.com>
2022-02-23 22:07:45 -0300

    Arrumandoos controllers

--- backEnd/controllers/eventos.js
+++ backEnd/controllers/eventos.js
@@ -1,57 +1,8 @@
-function renderizaHome(req, res, next) {
-  res.render('home')
-}
-
-function renderizaLogin(req, res, next) {
-  res.render('login')
-}
-
-function renderizaAjuda(req, res, next) {
-  res.render('ajuda')
-}
-
-function renderizaCadastro(req, res, next) {
-  res.render('cadastro')
-}
-
-function renderizaCarrinho(req, res, next) {
-  res.render('carrinho')
-}
-
-function renderizaCompraEfetuada(req, res, next) {
-  res.render('CompraEfetuada')
-}
-
-function renderizaIngressos(req, res, next) {
-  res.render('ingressos')
-}
-
-function renderizaPerfil(req, res, next) {
-  res.render('perfil')
-}
-
 function renderizaEventos(req, res, next) {
   res.render('eventos')
 }
 
-function renderizaShows(req, res, next) {
-  res.render('shows')
-}
 
 module.exports = {
-  renderizaHome,
-  renderizaLogin,
-  renderizaAjuda,
-  renderizaCadastro,
-  renderizaCarrinho,
-  renderizaCompraEfetuada,
-  renderizaIngressos,
-  renderizaPerfil,
   renderizaEventos,
-  renderizaShow01,
-  renderizaShow02,
-  renderizaShow03,
-  renderizaShow04,
-  renderizaShow05,
-  renderizaShow06
 }
