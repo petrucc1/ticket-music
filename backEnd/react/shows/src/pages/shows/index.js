2e2491c0d8889a87c364a853bd5391ea7f0b5202
mvrudies <marcos.rudies@live.com>
2022-04-04 20:11:13 -0300

    Inicio de react

---
+++ backEnd/react/shows/src/pages/shows/index.js
@@ -0,0 +1,16 @@
+import './style.css'
+
+     function Listacliente () { 
+
+          return (<div>
+                 <hl> Listagm de Clientes </hl>
+                    <li>Fulao</li>
+                    <li>Fulao</li>
+                    <li>Fulao</li>
+                    <li>Fulao</li>
+
+</div>)
+     } 
+
+
+ export default Listacliente
