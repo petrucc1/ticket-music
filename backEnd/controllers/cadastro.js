e0e0dc4eff36d6a8ea6958058c8d1154faa8de9f
mvrudies <marcos.rudies@live.com>
2022-04-14 00:20:54 -0300

    Atualização projeto

--- backEnd/controllers/cadastro.js
+++ backEnd/controllers/cadastro.js
@@ -1,10 +1,11 @@
-const usuario = require('../models/usuario')
+
+const cadastro = require('../models/cadastro')
 const axios = require('axios').default;
 
 async function create(req, res) {
   try {
-    const respostaCreateUsuario = await usuario.create(req.body)
-    return res.json(respostaCreateUsuario)
+    const respostaCreateCadastro = await usuario.create(req.body)
+    return res.json(respostaCreateCadastro)
   } catch (error) {
     return res.json({ message: error.message })
   }
